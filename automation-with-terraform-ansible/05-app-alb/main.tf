7c7c9f2e59b5ce7ce0334d766f58c17134a97289
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2024-01-24 19:27:54 +0530

    updated: ALB Listener

--- automation-with-terraform-ansible/05-app-alb/main.tf
+++ automation-with-terraform-ansible/05-app-alb/main.tf
@@ -11,3 +11,20 @@ resource "aws_lb" "app_alb" {
     var.tags
   )
 }
+
+# Create Listener for Application LoadBalancer
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hi, This response is from APP ALB"
+      status_code  = "200"
+    }
+  }
+}
