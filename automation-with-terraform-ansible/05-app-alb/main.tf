60f531d72af92d11b86a6921a922df4e18e1364d
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2024-01-24 17:56:10 +0530

    added: app ALB

---
+++ automation-with-terraform-ansible/05-app-alb/main.tf
@@ -0,0 +1,13 @@
+resource "aws_lb" "app_alb" {
+  name               = "${local.name}-${var.tags.Component}" # robotshop-dev-app-alb
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [data.aws_ssm_parameter.app_alb_sg_id.value]
+  subnets            = split(",",data.aws_ssm_parameter.private_subnet_ids.value) # StringList to List
+
+#   enable_deletion_protection = true
+  tags = merge(
+    var.common_tags,
+    var.tags
+  )
+}
