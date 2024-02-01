cc90174862264f038204275d01ca9204e27db61e
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2024-02-01 16:14:44 +0530

    web ALB

---
+++ 08-web-alb/variables.tf
@@ -0,0 +1,26 @@
+variable "common_tags" {
+  type = map
+  default = {
+    Project     = "robotshop"
+    Environment = "dev"
+    Terraform   = true
+  }
+}
+
+variable "project_name" {
+  default = "robotshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "tags" {
+  default = {
+    Component = "web-alb"
+  }
+}
+
+variable "zone_name" {
+  default = "rakeshintech.online"
+}
